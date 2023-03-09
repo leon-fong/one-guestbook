fe7d1d51f85c647b06bdeb6365a32f3e7413a637
fanglm <ooohmydawn@hotmail.com>
2023-03-09 10:15:36 +0800

    chore: add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider } from 'next-themes'
 import localFont from '@next/font/local'
@@ -7,12 +8,19 @@ const myFont = localFont({ src: '../styles/fonts/SmileySans.woff2' })
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <main className={myFont.className}>
-        <ThemeProvider attribute="class">
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </main>
-    </SessionProvider>
+    <>
+      <Script
+        strategy="afterInteractive"
+        async defer data-website-id="637b79c0-284a-4d38-b05c-dcf8836266da"
+        src={`https://stats.leonfong.me/app.js`}
+      />
+      <SessionProvider session={session}>
+        <main className={myFont.className}>
+          <ThemeProvider attribute="class">
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </main>
+      </SessionProvider>
+    </>
   )
 }
